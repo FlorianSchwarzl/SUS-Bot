d53a0f0ba9fd0afe517923fe9817c2699bcefa73
plastik-flasche <102389009+plastik-flasche@users.noreply.github.com>
2022-11-24 12:03:05 +0100

    removed a dot... that's it...

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
  * Config file for nonsensitive data
  */
 
-const fetchDataFromSave = require('../function/fetchDataFromSave.js');
+const fetchDataFromSave = require('./function/fetchDataFromSave.js');
 
 module.exports.christmasShit = ["christmas", "Weihnachten"];
 module.exports.prefix = "!";
