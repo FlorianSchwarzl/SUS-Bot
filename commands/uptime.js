6c6edf3c762890c156bbec7bb54572f249c1135f
roteKlaue <janoolhd@gmail.com>
2022-11-25 01:21:16 +0100

    remove useless code from uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -1,13 +1,5 @@
 const { MessageEmbed } = require('discord.js');
 
-const duration = (ms) => {
-    const sec = Math.floor((ms / 1000) % 60).toString();
-    const min = Math.floor((ms / (60 * 1000)) % 60).toString();
-    const hrs = Math.floor((ms / (60 * 60 * 1000)) % 60).toString();
-    const days = Math.floor((ms / (24 * 60 * 60 * 1000)) % 60).toString();
-    return `\`${days}Days\`,\`${hrs}Hours\`,\`${min}Minutes\`,\`${sec}Seconds\``;
-}
-
 module.exports = {
     "name": "uptime",
     "description": "",
