941a7a94bb82d476f2a9e7535c71a58f9068c403
plastik-flasche <102389009+plastik-flasche@users.noreply.github.com>
2022-11-26 19:54:43 +0100

    i'm scared

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,6 +1,8 @@
-const { welcomeMessages, welcomeChannelID } = require("../config");
+const fetchData = require("../config.js").fetchData;
+const welcomeChannel = fetchData.get("channels").welcome;
+const welcomeMessages = fetchData.get("messages").welcome;
 
 module.exports = (client, member) => {
-    const channel = client.channels.cache.get(welcomeChannelID);
-    channel.send(member.user.username + " " + welcomeMessages[Math.floor(Math.random() * welcomeMessages.length)]);
+    const channel = client.channels.cache.get(welcomeChannel);
+    channel.send(welcomeMessages[Math.floor(Math.random() * welcomeMessages.length)].replace("{user}", member.user.username + "#" + member.user.discriminator));
 }
--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,37 +1,27 @@
 const { Random } = require("sussyutilbyraphaelbader");
-const leonDetector = require("../function/leonDetector");
+const leonDetector = require("../function/leonDetector.js");
 const counter = require("../function/counter.js");
-const checkChannelID = require("../function/checkChannelID");
-const { allowedChannelsIDs, counterChannelID } = require("../config");
+const checkChannelID = require("../function/checkChannelID.js");
+const fetchData = require("../function/fetchDataFromSave.js");
+const channels = fetchData.get("channels");
 
-const patreon = [
-    'Please subscribe to our patreon.',
-    'Please subscribe to our OnlyFans.',
-    'Please subscribe to our OnlyFans. Our OnlyFans: https://onlyfans.com/stupid-useless-fans',
-    'Please subscribe to our patreon. Our patreon: https://www.patreon.com/stupid_useless_patreon',
-];
+const selfPromo = fetchData.get("messages").selfPromo;
 
 module.exports = (client, message) => {
-    if (message.author.bot) return;
-    if (counter(message, counterChannelID)) return;
-    if (leonDetector(message)) message.channel.send("Halts maul");
-    if (message.content.toLowerCase().includes("patreon")) return message.channel.send(patreon[Math.floor(Math.random() * patreon.length)]);
-    if (message.content.toLowerCase().includes("onlyfans")) return message.channel.send(patreon[Math.floor(Math.random() * patreon.length)]);
-    if (!checkChannelID(message, allowedChannelsIDs)) return;
+    if (message.author.bot) return;                                                 // Ignore bots
+    if (counter(message, channels.counter)) return;                                 // Check if the message is in the counter channel, if so, run the counter function
+    if (leonDetector(message)) message.channel.send("Halts maul");                  // We do a lil trolling
+    if (!checkChannelID(message, channels.allowed)) return;                         // Ignore messages not in allowed channels
 
     const prefix = client.config.prefix;
+    if (!message.content.startsWith(prefix)) return;                                // Ignore messages that don't start with the prefix
 
-    if (!message.content.startsWith(prefix)) return;
+    if (Random.randomInt(0, 9) === 0)                                               // 1/10 chance to send a self-promo message                         
+        message.channel.send(selfPromo[Random.randomInt(0, selfPromo.length - 1)]); // Shameless self-promotion
 
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
-    const cmd = client.commands.get(command);
-
-    const troll = Random.randomInt(0, 10);
-    if (troll > 7) {
-        message.channel.send(patreon[Math.floor(Math.random() * patreon.length)]);
-    }
-
-    if (!cmd) return;
-    cmd.run(client, message, args);
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);          // Get the arguments
+    const command = args.shift().toLowerCase();                                     // Get the command name
+    const cmd = client.commands.get(command);                                       // Get the command from the commands collection
+    if (!cmd) return;                                                               // Ignore commands that don't exist
+    cmd.run(client, message, args);                                                 // Run the command      
 }
