ba90d2c9707d218d85fb3d99b618a4ac7b1453d0
roteKlaue <janoolhd@gmail.com>
2022-11-27 11:08:42 +0100

    a

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,8 +1,7 @@
 const fetchData = require("../config.js").fetchData;
-const goodbyeChannel = fetchData.get("channels").goodbye;
 const goodbyeMessages = fetchData.get("messages").goodbye;
 
 module.exports = (client, member) => {
-	const channel = client.channels.cache.get(goodbyeChannel);
+	const channel = client.channels.cache.get(fetchData.get("channels").goodbye);
 	channel.send(goodbyeMessages[Math.floor(Math.random() * goodbyeMessages.length)].replace("{user}", member.user.username + "#" + member.user.discriminator));
 }
