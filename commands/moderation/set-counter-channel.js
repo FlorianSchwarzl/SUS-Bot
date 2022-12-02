8d86b02183d48105e4303ace487ddc53fff166bf
roteKlaue <janoolhd@gmail.com>
2022-12-02 21:18:05 +0100

    cleaned up some code / put channels into database

---
+++ commands/moderation/set-counter-channel.js
@@ -0,0 +1,37 @@
+const { ManageChannels } = require("../../enums/permissionBitField");
+const { ManageChannels: ManageChannel } = require("../../enums/permissionStrings");
+const getChannelFromMention = require('../../functions/getChannelFromMention');
+const guilds = require('../../schemas/guild');
+
+module.exports = {
+    name: 'set-counter-channel',
+    description: "Set the counter channel for",
+    aliases: ["scc"],
+
+    options: [
+        {
+            name: "channel",
+            type: "CHANNEL",
+            description: "The channel you want to set as counter channel.",
+            required: true
+        }
+    ],
+    
+    default_member_permissions: ManageChannel,
+
+    run: async(client, message, args, guildInfo, slash) => {
+        if (!slash) {
+            if (!message.member.permissions.has(ManageChannels)) {
+                message.delete();
+                return message.channel.send("You don't the required permissions to use this command.");
+            }
+        } else {
+            message.reply({ content: 'ok', ephemeral: true });
+        }
+
+        const channel = getChannelFromMention(message.guild, args[0]);
+        if(!channel) return message.channel.send("Please specify the counter channel.");
+
+        guilds.findByIdAndUpdate(guildInfo._id, { channels: { counter: channel.id, welcome: guildInfo.channels.welcome, goodbye: guildInfo.channels.goodbye   } }, (err, data) => {})
+    }
+}
