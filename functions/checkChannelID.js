0903409dcddb8893f06037f10b5d05f67ac581fc
plastik-flasche <102389009+plastik-flasche@users.noreply.github.com>
2022-12-06 22:06:45 +0100

    Revert "bug fix"
    This reverts commit 939d829bd695112beb5ee88780f3ea6767ec12b7.

--- functions/checkChannelID.js
+++ functions/checkChannelID.js
@@ -5,10 +5,8 @@ module.exports = (message, guildData) => {
     const allowedChannelsIDS = guildData.channels.allowed;
     if (Array.isArray(allowedChannelsIDS)) {
         if (allowedChannelsIDS.length === 0) return true;
-
         return allowedChannelsIDS.includes(message.channel.id + "");
     } else {
-        
-                        return allowedChannelsIDS === message.channel.id;
+        return allowedChannelsIDS === message.channel.id;
     }
 }
