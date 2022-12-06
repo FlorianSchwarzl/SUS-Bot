94713b8b3c89c398406896767eceadd9f2fe2c8c
plastik-flasche <102389009+plastik-flasche@users.noreply.github.com>
2022-12-06 22:12:44 +0100

    replaced ! with === undefined

--- functions/circle.js
+++ functions/circle.js
@@ -1,7 +1,7 @@
 const jimp = require("jimp");
 
 module.exports = async (image) => {
-    if (!image) throw new Error("You must provide an image as a first argument.");
+    if (image === undefined) throw new Error("You must provide an image as a first argument.");
     image = await jimp.read(image);
 
     image.resize(480, 480);
