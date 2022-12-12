27cd33e2c6f4c2b105a82e23f6c94f6411b74494
plastik-flasche <102389009+plastik-flasche@users.noreply.github.com>
2022-12-12 11:15:16 +0100

    fixed whatever tf that naming convention was

--- commands/economy/work.js
+++ commands/economy/work.js
@@ -1,14 +1,14 @@
 const userList = require("../../schemas/user");
 module.exports = {
-    name:"work",
-    description:"Work to earn money",
+    name: "work",
+    description: "Work to earn money",
     aliases: ["wk"],
- 
-    run (inputtedbot, email, variables, databases, benutzername, isASlashCommand) {
-        let earned = Math.round(Math.random()*400)+100;
-        benutzername.economy.wallet += earned;
-        userList.findByIdAndUpdate(benutzername._id, { economy: benutzername.economy }, (err, data) => { });
-        email.channel.send("Congratulations! You earned " + earned +  " gold. \nNow you have: " + benutzername.economy.wallet + "!");
+
+    run(client, message, args, guildData, userData, isSlashCommand) {
+        let earned = Math.round(Math.random() * 400) + 100;
+        userData.economy.wallet += earned;
+        userList.findByIdAndUpdate(userData._id, { economy: userData.economy }, (err, data) => { });
+        message.channel.send("Congratulations! You earned " + earned + " gold. \nNow you have: " + userData.economy.wallet + "!");
 
     }
 
