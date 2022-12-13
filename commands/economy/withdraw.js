fb39086cc28de42a26769916b1fe731d0c79d7ee
raphael cornelius bader <rapha.bader@gmail.com>
2022-12-13 21:43:02 +0100

    formatting fixes

--- commands/economy/withdraw.js
+++ commands/economy/withdraw.js
@@ -12,12 +12,16 @@ module.exports = {
     },
 
     run(client, message, args, guildData, userData, isSlashCommand) {
-        const amount = args[0];
-        if(amount && IsSomething.isNumber(amount) && amount <= userData.economy.bank) {
+        let amount = args[0];
+        if(amount && IsSomething.isNumber(amount)) {
+        if (amount > userData.economy.bank) amount = userData.economy.bank;
         userData.economy.bank -= +amount;
         userData.economy.wallet += +amount;
         userList.findByIdAndUpdate(userData._id, { economy: userData.economy }, (err, data) => { });
-        message.channel.send(amount + " deposited. You now have " + userData.economy.wallet + " gold in your wallet and " + userData.economy.bank + " gold in your bank");
+        message.channel.send(amount + " withdrawn. You now have " + userData.economy.wallet + " gold in your wallet and " + userData.economy.bank + " gold in your bank");
+        }
+        else {
+            message.channel.send("Please specify an amount");
         }
     }
 }
