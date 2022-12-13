06837af74dd075b501ba475bf26329aca7834e9a
plastik-flasche <102389009+plastik-flasche@users.noreply.github.com>
2022-12-13 21:40:57 +0100

    changed button requirements

--- buttons/play_pause.js
+++ buttons/play_pause.js
@@ -1,10 +1,12 @@
-module.exports = (client, interaction, guildData) => {
-    let returnValue = client.player.pause(interaction);
-    if (returnValue !== "The track is already paused") {
+module.exports = {
+    run(client, interaction, args, guildData, userData) {
+        let returnValue = client.player.pause(interaction);
+        if (returnValue !== "The track is already paused") {
+            interaction.reply({ content: returnValue, ephemeral: true });
+            return;
+        } else {
+            returnValue = client.player.resume(interaction);
+        }
         interaction.reply({ content: returnValue, ephemeral: true });
-        return;
-    } else {
-        returnValue = client.player.resume(interaction);
     }
-    interaction.reply({ content: returnValue, ephemeral: true });
 }
--- events/discord/interactionCreate.js
+++ events/discord/interactionCreate.js
@@ -4,40 +4,37 @@ const guildModel = require("../../schemas/guild");
 const userModel = require("../../schemas/user");
 
 module.exports = async (client, interaction) => {
+	let type = interaction.isCommand() ? "command" : interaction.isButton() ? "button" : "unknown";
 	let cmd;
 	let args = [];
-	if (interaction.isCommand()) {
-		cmd = client.commands.get(interaction.commandName);
-		args = interaction.options?._hoistedOptions.map(e => e.value);
-	}
-	else if (interaction.isButton()) {
-		console.log(interaction.customId);
-		if (interaction.customId.startsWith("command:")) {
-			console.log("command");
-			cmd = client.commands.get(interaction.customId.slice(8));
-			args = interaction.customId.slice(8).split(" ");
-			args.shift();
-		} else {
-			console.log("button");
-			try {
-				return client.buttons[interaction.customId](client, interaction, await getGuildData(interaction.guild.id));
-			} catch (e) {
-				console.log(e);
-				return interaction.reply({ content: "An unknown error occurred, sorry for the inconvenience!", ephemeral: true });
+	switch (type) {
+		case "command":
+			cmd = client.commands.get(interaction.commandName);
+			args = interaction.options?._hoistedOptions.map(e => e.value);
+			break;
+		case "button":
+			if (interaction.customId.startsWith("command:")) {
+				cmd = client.commands.get(interaction.customId.slice(8));
+				args = interaction.customId.slice(8).split(" ");
+				args.shift();
+			} else {
+				cmd = client.buttons[interaction.customId.split(" ")[0]];
+				args = interaction.customId.split(" ");
+				args.shift();
 			}
-		}
+			break;
+		default:
+			return;
 	}
-	else return;
-	console.log("prepares to run command");
 	interaction.channel = client.channels.cache.get(interaction.channelId);
 	interaction.author = interaction.user;
 	const guildData = await getGuildData(interaction.guild.id);
 	const userData = await getUserData(interaction.user.id);
-	if (cmd) {
-		let returnValue = cmd.run(client, interaction, args, guildData, userData, true);
-		if (returnValue instanceof Promise) returnValue = await returnValue;
-		if ((typeof returnValue === "string" && returnValue !== "") || returnValue?.embeds !== undefined) interaction.channel.send(returnValue);
-	}
+
+	let returnValue = cmd.run(client, interaction, args, guildData, userData, true)
+
+	if (returnValue instanceof Promise) returnValue = await returnValue;
+	if ((typeof returnValue === "string" && returnValue !== "") || returnValue?.embeds !== undefined) interaction.channel.send(returnValue);
 }
 
 async function getGuildData(guildId) {
