410773e1f22d842d146bba1331ffb30c3607bc5e
roteKlaue <janoolhd@gmail.com>
2022-12-14 22:36:12 +0100

    bot info command updated

--- commands/general/info.js
+++ commands/general/info.js
@@ -5,24 +5,27 @@ module.exports = {
     name: "info",
     description: "Displays information about the bot.",
 
-    run(client, message, args, guildData, userData, isSlashCommand) {
-        message.channel.send({
+    run(client, message) {
+        message.reply({
             embeds: [new MessageEmbed()
                 .setColor("BLUE")
                 .setTitle(`${client.user.username} v${require("../../package.json")["version"]}`)
                 .setThumbnail(client.user.displayAvatarURL({ dynamic: true }))
                 .setURL("https://github.com/plastik-flasche/SUS-Bot")
-                .addField("Uptime", `${ms(client.uptime)}`, true)
-                .addField("WebSocket Ping", `${client.ws.ping}ms`, true)
-                .addField("Discord.js", `${discordjsVersion}`, true)
-                .addField("Guild Count", `${client.guilds.cache.size} guilds`, true)
-                .addField("User Count", `${client.users.cache.size} users`, true)
-                .addField("Commands", `${client.commands.size} commands`, true)
-                .addField("Memory", `${(process.memoryUsage().rss / 1024 / 1024).toFixed(2)} MB RSS\n${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB Heap`, true)
-                .addField("Cached Data", `${client.users.cache.size} users\n${client.emojis.cache.size} emojis`, true)
-                .addField("Node", `${process.version} on ${process.platform} ${process.arch}`, true)
+                .addFields(
+                    { name: "Uptime", value: `${ms(client.uptime)}`, inline: true },
+                    { name: "WebSocket Ping", value: `${client.ws.ping}ms`, inline: true },
+                    { name: "Discord.js", value:`${discordjsVersion}`, inline: true },
+                    { name: "Guild Count", value: `${client.guilds.cache.size} guilds`, inline: true },
+                    { name: "User Count", value: `${client.users.cache.size} users`, inline: true },
+                    { name: "Commands", value: `${client.commands.size} commands`, inline: true },
+                    { name: "Memory", value: `${(process.memoryUsage().rss / 1024 / 1024).toFixed(2)} MB RSS\n${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB Heap`, inline: true },
+                    { name: "Cached Data", value:`${client.users.cache.size} users\n${client.emojis.cache.size} emojis`, inline: true },
+                    { name: "Node", value: `${process.version} on ${process.platform} ${process.arch}`, inline: true }
+                )
                 .setFooter(client.config.embedFooter(client))
-                .setTimestamp(new Date())]
+                .setTimestamp(new Date())
+            ]
         })
     }
 }
