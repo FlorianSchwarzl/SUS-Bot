18b573f2c6bfefe89b1af18dc945dde287ebc6a7
plastik-flasche <102389009+plastik-flasche@users.noreply.github.com>
2023-01-05 23:06:05 +0100

    inviteBot now return something

--- commands/general/inviteBot.js
+++ commands/general/inviteBot.js
@@ -3,6 +3,6 @@ module.exports = {
     description: "Sends the invite link of the bot",
 
     async run(client, message, args, guildData, userData, isSlashCommand) {
-        message.channel.send({ content: "https://discord.com/api/oauth2/authorize?client_id=" + client.user.id + "&permissions=8&scope=bot%20applications.commands" });
+        return { content: "https://discord.com/api/oauth2/authorize?client_id=" + client.user.id + "&permissions=8&scope=bot%20applications.commands" };
     }
 }
