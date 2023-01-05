9daca05f43552da9b8e74da680bb64bf01f0ec1c
plastik-flasche <102389009+plastik-flasche@users.noreply.github.com>
2023-01-05 20:39:46 +0100

    fixed bot crashing when command takes too long to respond

--- functions/formatCommandReturn.js
+++ functions/formatCommandReturn.js
@@ -1,18 +1,20 @@
-module.exports = async (returnValue) => {
-    if (returnValue instanceof Promise) {
-        const timeout = setTimeout(() => {
-            throw new Error("Command took too long to execute!");
-        }, 4500);
-        returnValue = await returnValue;
-        clearTimeout(timeout);
-    }
-    if ((typeof returnValue === "string" && returnValue !== "")
-        || returnValue?.embeds !== undefined
-        || returnValue?.files !== undefined
-        || returnValue?.components !== undefined
-        || returnValue?.content !== undefined) {
-        if (typeof returnValue === "string") returnValue = { content: returnValue };
-        returnValue.ephemeral = true;
-    } else throw new Error(`Command returned nothing.`);
-    return returnValue;
+module.exports = async (returnValue, command) => {
+    return await new Promise(async (resolve, reject) => {
+        if (returnValue instanceof Promise) {
+            const timeout = setTimeout(() => {
+                reject(`Command ${command.name} took too long to execute!`);
+            }, 4500);
+            returnValue = await returnValue;
+            clearTimeout(timeout);
+        }
+        if ((typeof returnValue === "string" && returnValue !== "")
+            || returnValue?.embeds !== undefined
+            || returnValue?.files !== undefined
+            || returnValue?.components !== undefined
+            || returnValue?.content !== undefined) {
+            if (typeof returnValue === "string") returnValue = { content: returnValue };
+            returnValue.ephemeral = true;
+        } else reject(`Command "${command.name}" returned nothing.`);
+        resolve(returnValue);
+    });
 }
