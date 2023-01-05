f52ceba822eba29a943a8a470c9104fd6b1793b5
plastik-flasche <102389009+plastik-flasche@users.noreply.github.com>
2023-01-06 00:44:14 +0100

    now with 100% less crashes (in this file, I hope)

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -14,10 +14,12 @@ module.exports = {
         let currentString = `Current: **${playerInfo.current.title}**\n`;
         let queueString = "";
 
-        for (let i = 0; i < playerInfo.queue.length; i++) {
+        for (let i = 0; i < Math.min(playerInfo.queue.length, 18); i++) {
             const track = playerInfo.queue[i];
             queueString += `${i + 1}. **${track.title}**\n`;
         }
+        if (playerInfo.queue.length > 18)
+            queueString += `And ${playerInfo.queue.length - 18} more...`;
 
         return (currentString + queueString);
     }
