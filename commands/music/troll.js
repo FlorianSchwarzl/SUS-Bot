e60c2859c1b86727370079b631bf7db495413bec
plastik-flasche <102389009+plastik-flasche@users.noreply.github.com>
2023-01-06 00:43:39 +0100

    added troll logging bc roteKlaue keeps trolling me :(

--- commands/music/troll.js
+++ commands/music/troll.js
@@ -8,6 +8,8 @@ module.exports = {
     run(client, message, args, guildData, userData, isSlashCommand) {
         if (!isSlashCommand) message.delete();
 
+        // Low the user using the command
+        console.log(`${message.author.username} used the troll command.`);
         return client.player.troll(message);
     }
 }
