e178f43b5b5240c1b069a89c861f86fc71fe50ad
plastik-flasche <102389009+plastik-flasche@users.noreply.github.com>
2023-01-06 02:42:56 +0100

    why was this even here?

--- config.js
+++ config.js
@@ -2,7 +2,6 @@
  * Config file for nonsensitive data
  */
 
-module.exports.christmasShit = ["christmas", "Weihnachten"];
 module.exports.prefix = process.env.PREFIX;
 module.exports.fetchData = require("./functions/fetchDataFromSave.js");
 module.exports.embedFooter = (client) => ({ text: `Running as ${client.user.tag} on version ${require("./package.json")["version"]} of the SUS-Bot by ${require("./package.json")["authors"].join(" and ")}` });
