30172b17cf3d4465770de03cb2518603598781fe
plastik-flasche <102389009+plastik-flasche@users.noreply.github.com>
2023-01-06 01:48:26 +0100

    deleted LeonDetector cause that's gonna get us in trouble some day

--- functions/leonDetector.js
+++
@@ -1,3 +0,0 @@
-module.exports = (message) => {
-    return message.author.id === "583743858865602702" || message.author.id === "696838136877154364" || message.author.id === "769189119951372328"; // leon detector
-}
