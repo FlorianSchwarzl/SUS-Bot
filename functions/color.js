e8b23ba0c8119366a86077269e2b99611bc236ae
plastik-flasche <102389009+plastik-flasche@users.noreply.github.com>
2023-01-08 14:16:21 +0100

    deleted unnecessary functions

--- functions/color.js
+++
@@ -1,9 +0,0 @@
-const { createCanvas } = require("canvas");
-
-module.exports = (color = "#fff") => {
-    const canvas = createCanvas(2048, 2048);
-    const ctx = canvas.getContext("2d");
-    ctx.fillStyle = color;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    return canvas.toBuffer();
-}
