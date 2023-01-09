72e90ca61b98daaf5f2884dd82e8c577f3d452af
plastik-flasche <102389009+plastik-flasche@users.noreply.github.com>
2023-01-09 15:25:16 +0100

    fixed interactions

--- events/discord/interactionCreate.js
+++ events/discord/interactionCreate.js
@@ -8,20 +8,19 @@ module.exports = async (client, interaction) => {
 	if (interaction.customId)
 		interaction.customId = interaction.customId.toLowerCase();
 
-	let type = interaction.type;
+	let type;
 
-	if (type === 3) // INTERACTION_RESPONSE
-		if (interaction.componentType === 2) // BUTTON
-			type = 32;
-		else if (interaction.componentType === 3) // SELECT_MENU
-			type = 33;
+	if (interaction.isCommand()) type = "COMMAND";
+	else if (interaction.isButton()) type = "BUTTON";
+	else if (interaction.isSelectMenu()) type = "SELECT_MENU";
+	else throw new Error("Unknown interaction type");
 
 	switch (type) {
-		case 2: // COMMAND
+		case "COMMAND":
 			cmd = client.commands.get("command:" + interaction.commandName);
 			args = interaction.options?._hoistedOptions.map(e => e.value);
 			break;
-		case 32: // BUTTON
+		case "BUTTON":
 			if (interaction.customId.startsWith("command:")) {
 				cmd = client.commands.get("command:" + interaction.customId.slice(8));
 				args = interaction.customId.slice(8).split(" ");
@@ -33,7 +32,7 @@ module.exports = async (client, interaction) => {
 			}
 			isComponent = true;
 			break;
-		case 33: // SELECT_MENU
+		case "SELECT_MENU":
 			cmd = client.commands.get("selectMenu:" + interaction.customId);
 			args = interaction.customId.split(" ");
 			args[0] = interaction.values[0];
